13af10e69564f896589b56eec99f728dd76abeb8
Coleman Adkins <ladiesluvcoleman@yahoo.com>
2022-01-07 14:09:34 -0500

    Add columns and rows

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 // I want to have a daily date on the page
+$("#currentDay").text(moment().format("MMM Do YY")); 
+
 // I want the time block colors to change with the time
+
 // I want to save to local storage so when I refresh the page the input stays
+
 // Alert message: "I just saved to local storage"
 
-$("#currentDay").text(moment().format("MMM Do YY")); 
